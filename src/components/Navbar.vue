ea6663e846c645bffd952b68c98ba4f655e05839
Lxmune <lamuunya@gmail.com>
2023-04-20 10:53:37 +0200

    Ajout de fichiers

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav class="navbar">
+    
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  data() {
+  },
+  methods: {
+  }
+};
+</script>
+
+<style>
+
+</style>
