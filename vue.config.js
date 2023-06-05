3dac2db59c4329a1c75f14ebfde99a687535f7db
Lamune <d.tom2090@gmail.com>
2023-06-05 15:27:41 +0200

    prod

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+
+const publicPath = process.env.NODE_ENV === 'production' ? '/Frontend/' : '/'
+
+module.exports = {
+  publicPath: publicPath,
+  ...defineConfig({
+    transpileDependencies: true
+  })
+};
